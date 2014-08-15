39b4ae2e9d2d02bc40847f984e0852158da794e0
edinantox <edinantox@gmail.com>
2014-08-15 01:15:51 -0300

    .

---
+++ model/classes/Conexao.class.php
@@ -0,0 +1,18 @@
+<?php
+class Conexao{
+	function conectar(){
+		$con=new mysqli("localhost","site","site","phoenixlight");
+		if ($con->connect_error) {
+			die('Connect Error (' . $mysqli->connect_errno . ') '. $con->connect_error);
+		}
+		return $con;
+	}
+
+	function query($qr){
+		$con=$this->conectar();
+		$res=$con->query($qr) or die($qr."<br>".mysql_error());
+		$con->close();
+		return $res;
+	}
+}
+?>
