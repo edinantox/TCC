df980c47fc0e6febad417ae97fef9ddf4091c24f
edinantox <edinantox@gmail.com>
2014-09-02 18:36:01 -0300

    .

--- control/paginas/main.php
+++ control/paginas/main.php
@@ -1,4 +1,31 @@
 <?php
+echo '
+    <div id="content">
 
+        <div id="logo">
+        </div>
+
+        <div id="header">
+        </div>
+
+        <div id="search">
+        </div>
+
+        <div id="cat">
+        </div>
+
+        <div id="banner">
+        </div>
+
+        <div id="principais">
+        </div>
+
+        <div id="footer">
+        </div>
+    </div>
+
+
+
+';
 ?>
 
